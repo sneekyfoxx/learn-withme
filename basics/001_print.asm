228507958bcf5504b5016615908ea58de8c30f94
sneekyfoxx <sneekyfoxx09@gmail.com>
2024-02-10 03:03:49 -0500

    Update 001_print.asm

--- basics/001_print.asm
+++ basics/001_print.asm
@@ -1,23 +1,24 @@
-  format ELF64 executable 3
+  format ELF64 executable 3      ; declare the binary format as an ELF 64-bit executable for Linux (3)
 
-  segment readable executable
+  segment readable executable    ; code segment
 
-  entry main
+  entry main                     ; declare the program entry point
 
   main:
-    mov    rax, 5            ; put 5 into rax register
-    add    rax, 48           ; add 48 to rax  (5)
-    mov    [number], rax     ; put rax (5) into variable number
-    mov    rax, 1            ;)
-    mov    rdi, 1            ; stdout
-    mov    rsi, number       ; buffer
-    mov    rdx, 1            ; sys_write(
-    syscall
+    mov    rax, 5                ; copy 5 into rax (accumulator register)
+    add    rax, 48               ; add 48 (ASCII value 0) to the value in rax 
+    mov    [number], rax         ; copy the value in rax into the variable (memory address) named number
 
-    mov    rax, 60
-    mov    rdi, 0
-    syscall
+    mov    rax, 1                ; sys_write (
+    mov    rdi, 1                ; stdout file descriptor
+    mov    rsi, number           ; character buffer
+    mov    rdx, 1                ; buffer length
+    syscall                      ; )
+
+    mov    rax, 60               ; sys_exit (
+    mov    rdi, 0                ; exit code
+    syscall                      ; )
   
-  segment readable writable
-  
-  number dq "", 0x00
+  segment readable writable      ; data segment 
+
+    number dq ""                 ; define a variable named number of size quadword (8 bytes)
